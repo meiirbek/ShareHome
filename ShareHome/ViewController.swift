3aff51aeba8173d7b6fd1f3499c49459ae0681ea
meiirbek <meiirbek@meiirbeks-macbook-pro.local>
2017-03-16 04:00:36 -0500

    Added "burger" button image.

--- ShareHome/ViewController.swift
+++ ShareHome/ViewController.swift
@@ -10,23 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var open: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        open.target = self.revealViewController()
-        open.action = #selector(SWRevealViewController.revealToggle(_:))
-        
+        let button = UIButton.init(type: .custom)
+        button.setImage(UIImage(named: "burger.png"), for: UIControlState.normal)
+        button.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: UIControlEvents.touchUpInside)
+        button.frame = CGRect(x: 0, y: 0, width: 53, height: 51)
+        let barButton = UIBarButtonItem(customView: button)
+        self.navigationItem.leftBarButtonItem = barButton
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
-        
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
