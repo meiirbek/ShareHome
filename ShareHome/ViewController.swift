adb327040627fc49757885be321eea450b94a705
meiirbek <meiirbek@Meiirbeks-MacBook-Pro.local>
2017-03-16 16:12:20 -0500

    changed the burger icon and added a title

--- ShareHome/ViewController.swift
+++ ShareHome/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         let button = UIButton.init(type: .custom)
-        button.setImage(UIImage(named: "burger.png"), for: UIControlState.normal)
+        button.setImage(UIImage(named: "white.png"), for: UIControlState.normal)
         button.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: UIControlEvents.touchUpInside)
         button.frame = CGRect(x: 0, y: 0, width: 53, height: 51)
         let barButton = UIBarButtonItem(customView: button)
