b232e3113490a889c065a1e5f6b42f40e92d2f16
meiirbek <meiirbek@Meiirbeks-MacBook-Pro.local>
2017-04-05 23:09:31 -0500

    CollectionView for Posts have been partially implemented. Still need to define the number of sections and row for cell. Also, another prototype type of cell, for anonymous posts, is necessary.

--- ShareHome/BackTableVC.swift
+++ ShareHome/BackTableVC.swift
@@ -13,7 +13,7 @@ class BackTableVC: UITableViewController {
     var TableArray = [String] ()
     
     override func viewDidLoad() {
-        TableArray = ["Main Page","Personal info","Make a Post"]
+        TableArray = ["Home","Profile","Make a Post"]
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
