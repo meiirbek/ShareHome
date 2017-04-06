f0ab3826ec9b0913ff9a7b4c6ea39742e717166d
meiirbek <meiirbek@Meiirbeks-MacBook-Pro.local>
2017-04-06 15:36:47 -0500

    Have added controllers for Schedule, Grocery List and Purchases

--- ShareHome/BackTableVC.swift
+++ ShareHome/BackTableVC.swift
@@ -13,7 +13,7 @@ class BackTableVC: UITableViewController {
     var TableArray = [String] ()
     
     override func viewDidLoad() {
-        TableArray = ["Home","Profile","Make a Post"]
+        TableArray = ["Home","Profile","Make a Post", "Schedule","Grocery List","Purchases"]
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -21,7 +21,7 @@ class BackTableVC: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCell(withIdentifier: TableArray[indexPath.row], for: indexPath) as UITableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: TableArray[indexPath.row], for: indexPath) as UITableViewCell
         
         cell.textLabel?.text = TableArray[indexPath.row]
 
