893482a1b2a6e30f015bb741c2a343ca832745b8
meiirbek <meiirbek@Meiirbeks-MacBook-Pro.local>
2017-04-27 15:52:21 -0500

    preparation step for initial view. Not a lot has changed

--- initialLoginVC.swift
+++ initialLoginVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
 class initialLoginVC: UIViewController {
 
@@ -18,14 +19,21 @@ class initialLoginVC: UIViewController {
     @IBAction func signUpPressed(_ sender: UIButton) {
         UserDefaults.standard.set(loginName.text, forKey: "username")
         performSegue(withIdentifier: "toMainSegue", sender: self)
-        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         
         signUpButton.layer.cornerRadius = 5
         signUpButton.clipsToBounds = true
-
+        
+        let loginButton = FBSDKLoginButton()
+        loginButton.center = self.view.center
+        self.view.addSubview(loginButton)
+        facebookButtonPressed(button: loginButton)
+    }
+    
+    @IBAction func facebookButtonPressed(button: UIButton) {
+        
     }
 
     
